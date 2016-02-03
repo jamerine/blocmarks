a1c78e1db697e1622d0a2a50fedefa55cfea47bf
Jason Amerine <amerine.19@gmail.com>
2016-02-03 14:08:57 -0500

    User Story 1 Sign Up: Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: 'amerine.19@gmail.com', password: 'password')
